e4b3cb398a878e5e92d3a14b90987513058684bd
xenon54v <kokona955@gmail.com>
2026-02-20 15:11:11 +0300

    add zaglushkafor spr 5

---
+++ src/gui/widgets/audit_log_viewer.py
@@ -0,0 +1,23 @@
+﻿import tkinter as tk
+from tkinter import ttk
+
+
+class AuditLogViewer(tk.Toplevel):
+    """
+    Stub window for Sprint 5 audit log viewer.
+    """
+
+    def __init__(self, master=None):
+        super().__init__(master)
+
+        self.title("Audit Log (Stub)")
+        self.geometry("600x400")
+
+        label = tk.Label(self, text="Audit Log Viewer will be implemented in Sprint 5")
+        label.pack(pady=10)
+
+        self.table = ttk.Treeview(self, columns=("Time", "Action", "Entry"), show="headings")
+        self.table.heading("Time", text="Time")
+        self.table.heading("Action", text="Action")
+        self.table.heading("Entry", text="Entry")
+        self.table.pack(fill=tk.BOTH, expand=True)
